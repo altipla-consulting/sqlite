b9b0fc49bf7245a053dd842f879746febd9ff64a
Ernesto Alejo <ernestokarim@gmail.com>
2023-03-13 08:02:38 +0100

    Configure a busy timeout for memory databases.

--- sqlite.go
+++ sqlite.go
@@ -11,7 +11,7 @@ import (
 
 func Open(dsn string) (*sqlx.DB, error) {
 	if dsn == ":memory:" {
-		dsn = "file::memory:?mode=memory&cache=shared"
+		dsn = "file::memory:?mode=memory&cache=shared&_timeout=5000"
 	} else {
 		if err := os.MkdirAll(filepath.Dir(dsn), 0700); err != nil {
 			return nil, fmt.Errorf("cannot create data directory: %w", err)
