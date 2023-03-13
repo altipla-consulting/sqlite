0f415202a8ca14b6ef10e50f71e7211caf248fcd
Ernesto Alejo <ernestokarim@gmail.com>
2023-03-13 20:11:58 +0100

    Configure file database using WAL and memory database using a shared cache.

--- sqlite.go
+++ sqlite.go
@@ -10,21 +10,25 @@ import (
 )
 
 func Open(dsn string) (*sqlx.DB, error) {
+	var connect string
 	if dsn == ":memory:" {
-		dsn = "file::memory:?mode=memory&_timeout=5000"
+		dsn = "file::memory:?mode=memory"
 	} else {
 		if err := os.MkdirAll(filepath.Dir(dsn), 0700); err != nil {
 			return nil, fmt.Errorf("cannot create data directory: %w", err)
 		}
-		dsn += "?_timeout=5000&_fk=true&_journal=WAL&_synchronous=NORMAL"
+		connect = "file:" + dsn + "?_timeout=5000&_fk=true&_journal=WAL&_synchronous=NORMAL&mode=rwc&cache=private"
 	}
-	log.WithField("dsn", dsn).Debug("Open SQLite3 connection")
-	db, err := sqlx.Open("sqlite3", dsn)
+	log.WithField("dsn", connect).Debug("Open SQLite3 connection")
+	db, err := sqlx.Open("sqlite3", connect)
 	if err != nil {
 		return nil, fmt.Errorf("cannot open database: %w", err)
 	}
 
-	db.SetMaxOpenConns(1)
+	if dsn == ":memory:" {
+		db.SetMaxOpenConns(1)
+	}
+
 	db.MapperFunc(func(s string) string { return s })
 
 	return db, nil
