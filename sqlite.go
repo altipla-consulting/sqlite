7af88d82788d5d4705eb26796b484637c6bae1c1
Ernesto Alejo <ernestokarim@gmail.com>
2023-03-13 13:50:21 +0100

    Limit to 1 connection removing the deprecated shared cache.

--- sqlite.go
+++ sqlite.go
@@ -11,12 +11,12 @@ import (
 
 func Open(dsn string) (*sqlx.DB, error) {
 	if dsn == ":memory:" {
-		dsn = "file::memory:?mode=memory&cache=shared&_timeout=5000"
+		dsn = "file::memory:?mode=memory&_timeout=5000"
 	} else {
 		if err := os.MkdirAll(filepath.Dir(dsn), 0700); err != nil {
 			return nil, fmt.Errorf("cannot create data directory: %w", err)
 		}
-		dsn += "?_timeout=5000&_fk=true&_journal=WAL&_synchronous=NORMAL&cache=shared"
+		dsn += "?_timeout=5000&_fk=true&_journal=WAL&_synchronous=NORMAL"
 	}
 	log.WithField("dsn", dsn).Debug("Open SQLite3 connection")
 	db, err := sqlx.Open("sqlite3", dsn)
@@ -24,6 +24,7 @@ func Open(dsn string) (*sqlx.DB, error) {
 		return nil, fmt.Errorf("cannot open database: %w", err)
 	}
 
+	db.SetMaxOpenConns(1)
 	db.MapperFunc(func(s string) string { return s })
 
 	return db, nil
