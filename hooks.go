f4bf2ccce35cd8ee3ec11bc6918017d983c39a91
Ernesto Alejo <ernestokarim@gmail.com>
2023-03-19 12:00:05 +0100

    Publish hook interfaces to test implementations against them.

--- hooks.go
+++ hooks.go
@@ -12,12 +12,12 @@ type Hooks[T any] struct {
 
 type Hook[T any] func(ctx context.Context, model *T) error
 
-type hookerBeforePut[T any] interface {
+type HookBeforePut[T any] interface {
 	BeforePut(ctx context.Context, tx *Tx[T]) error
 }
 
 func runBeforePut[T any](ctx context.Context, tx *Tx[T], hooks Hooks[T], model *T) error {
-	if hook, ok := any(model).(hookerBeforePut[T]); ok {
+	if hook, ok := any(model).(HookBeforePut[T]); ok {
 		if err := hook.BeforePut(ctx, tx); err != nil {
 			return fmt.Errorf("before put hook: %w", err)
 		}
@@ -32,12 +32,12 @@ func runBeforePut[T any](ctx context.Context, tx *Tx[T], hooks Hooks[T], model *
 	return nil
 }
 
-type hookerAfterPut interface {
+type HookAfterPut interface {
 	AfterPut(ctx context.Context) error
 }
 
 func runAfterPut[T any](ctx context.Context, hooks Hooks[T], model *T) error {
-	if hook, ok := any(model).(hookerAfterPut); ok {
+	if hook, ok := any(model).(HookAfterPut); ok {
 		if err := hook.AfterPut(ctx); err != nil {
 			return fmt.Errorf("after put hook: %w", err)
 		}
