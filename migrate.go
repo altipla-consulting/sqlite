ff3b07702ec8c8553310f25e6d8fd7e78d11e8d2
ErnestNeller <ernest.neller@altiplaconsulting.com>
2024-06-19 17:53:36 +0200

    Add migrations.

---
+++ migrate.go
@@ -0,0 +1,36 @@
+package sqlite
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Migration func(ctx context.Context, db *sqlx.DB) error
+
+func Migrate(ctx context.Context, db *sqlx.DB, migrations []Migration) error {
+	var version int64
+	if err := db.GetContext(ctx, &version, "PRAGMA user_version"); err != nil {
+		return err
+	}
+
+	if version >= int64(len(migrations)) {
+		return nil
+	}
+
+	slog.Info("Running migrations", slog.Int64("from", version), slog.Int("to", len(migrations)))
+	for index, migration := range migrations[version:] {
+		slog.Info("Run migration", slog.Int("version", index+1))
+
+		if err := migration(ctx, db); err != nil {
+			return err
+		}
+		if _, err := db.ExecContext(ctx, fmt.Sprintf("PRAGMA user_version = %v", index+1)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
